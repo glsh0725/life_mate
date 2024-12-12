8a0b8e47bec3755f6a53ec0a9f699e6742fb3b4d
glsh0725 <lsh920725@naver.com>
2024-12-13 04:51:47 +0900

    mate

--- src/main/resources/static/js/mate/mate.js
+++ src/main/resources/static/js/mate/mate.js
@@ -183,6 +183,11 @@ document.addEventListener("DOMContentLoaded", () => {
                 modal.querySelector(".close-btn").addEventListener("click", () => {
                     modal.style.display = "none";
                 });
+                modal.addEventListener("click", (event) => {
+                    if (event.target === modal) {
+                        modal.style.display = "none";
+                    }
+                });
                 modal.style.display = "block";
             });
         });
